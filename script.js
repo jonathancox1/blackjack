13e22dfdaf655c6ae7faed13e80b4bde7c181ef3
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-23 21:04:46 -0400

    random cards generated

--- script.js
+++ script.js
@@ -24,25 +24,6 @@ const deck = createDeck();
 // get deal button
 const dealButton = document.querySelector("#deal-button");
 
-// get button parent <div> class
-const buttons = document.querySelector(".buttons");
-// add event listener to parent <div> class = 'buttons'
-buttons.addEventListener("click", function (e) {
-  console.log(event.target.id);
-  if (event.target.id === "deal-button") {
-    renderCards(playersHand, hitMe(getCardImageUrl(getCard(playerScore))));
-    renderCards(playersHand, hitMe(getCardImageUrl(getCard(playerScore))));
-    renderCards(dealersHand, hitMe(getCardImageUrl(getCard(dealerScore))));
-    renderCards(dealersHand, hitMe(getCardImageUrl(getCard(dealerScore))));
-  } else if (event.target.id === "hit-button") {
-    renderCards(playersHand, hitMe(getCardImageUrl(getCard(playerScore))));
-  } else {
-    renderCards(dealersHand, hitMe(getCardImageUrl(getCard(dealerScore))));
-    console.log(dealerScore);
-    console.log(playerScore);
-  }
-});
-
 // get hit button
 const hitButton = document.querySelector("#hit-button");
 // add event listener
@@ -53,13 +34,23 @@ const dealersHand = document.querySelector("#dealer-hand");
 // get players hand
 const playersHand = document.querySelector("#player-hand");
 
-// render 2 cards Deal function
-function dealCards() {
-  return `
-    <img src="images/4_of_clubs.png" width="80px">
-    <img src="./images/6_of_spades.png" width="80px">
-    `;
-}
+// get button parent <div> class
+const buttons = document.querySelector(".buttons");
+// add event listener to parent <div> class = 'buttons'
+buttons.addEventListener("click", function (e) {
+  if (event.target.id === "deal-button") {
+    renderCards(playersHand, hitMe(getCard(playerScore)));
+    renderCards(playersHand, hitMe(getCard(playerScore)));
+    renderCards(dealersHand, hitMe(getCard(dealerScore)));
+    renderCards(dealersHand, hitMe(getCard(dealerScore)));
+  } else if (event.target.id === "hit-button") {
+    renderCards(playersHand, hitMe(getCard(playerScore)));
+  } else {
+    renderCards(dealersHand, hitMe(getCard(dealerScore)));
+    console.log(dealerScore);
+    console.log(playerScore);
+  }
+});
 
 // render cards to corresponding div
 function renderCards(who, dealOrHit) {
@@ -71,35 +62,53 @@ function renderCards(who, dealOrHit) {
 // render 1 card Hit function
 function hitMe(url) {
   return `
-    <img src="${url}" width="80px">
+    <img src="${url}" width="80px" stye="padding: 0px 20px 0px 20px;">
     `;
 }
 
 // get the card Image
 function getCardImageUrl(card) {
   if (card.point < 2) {
-    console.log(card.suit, card.point);
+    console.log(card.point + "ace");
     return `images/ace_of_${card.suit}.png`;
   } else if (card.point < 10) {
-    console.log(card.suit, card.point);
+    console.log(card.point + "point");
     return `images/${card.point}_of_${card.suit}.png`;
   } else if (card.point === 11) {
-    console.log(card.suit, card.point);
+    console.log(card.point + "jack");
     return `images/jack_of_${card.suit}.png`;
   } else if (card.point === 12) {
-    console.log(card.suit, card.point);
+    console.log(card.point + "queen");
     return `images/queen_of_${card.suit}.png`;
   } else if (card.point === 13) {
-    console.log(card.suit, card.point);
+    console.log(card.point + "king");
     return `images/king_of_${card.suit}.png`;
   }
 }
+
+// which card from the deck
+// appends the card value to the score array
+function getCard(who) {
+  let rand = Math.floor(Math.random() * (deck[0].length - 1));
+  console.log("random number" + [rand]);
+  // let removed = deck[0].splice(rand);
+  // console.log(removed);
+  return getCardImageUrl(deck[0][rand]);
+}
+
+// // remove from the deck
+// function removeFromDeck(card) {
+//   console.log(card);
+//   delete deck.card;
+// }
 
 const playerScore = [];
 const dealerScore = [];
 
 //player points
 const playerPoints = document.querySelector("#player-points");
+
+// sum points function
 function sumPlayer() {
   let sum = 0;
   for (let i = 0; i < playerScore.length; i++) {
@@ -114,11 +123,3 @@ playerPoints.textContent = sumPlayer();
 const dealerPoints = document.querySelector("#dealer-points");
 const sumDealer = dealerScore.reduce((acc, val) => acc + val, 0);
 dealerPoints.textContent = sumDealer;
-
-// which card from the deck
-// appends the card value to the score array
-function getCard(who) {
-  let rand = Math.floor(Math.random() * 53);
-  who.push(deck[0][rand].point);
-  return deck[0][rand];
-}
